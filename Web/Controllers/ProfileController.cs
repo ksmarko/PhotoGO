d8cc3385d5620d5ce3a77d143ca614abc7b8c0fd
Ksenia <ks.markodev@gmail.com>
2018-04-28 23:29:08 +0300

    Simple navigation at user profile page and core for album page

---
+++ Web/Controllers/ProfileController.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using BLL.DTO;
+using BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class ProfileController : Controller
+    {
+        readonly IMediaService mediaService;
+
+        public ProfileController (IMediaService ms)
+        {
+            mediaService = ms;
+        }
+
+        public ActionResult Index()
+        {
+            return View("UserProfile");
+        }
+
+        public ActionResult Albums()
+        {
+            var albums = new List<AlbumModel>()
+            {
+                new AlbumModel() {Name = "Name1", Description="Description1"},
+                new AlbumModel() {Name = "Name2", Description="Description2"},
+                new AlbumModel() {Name = "Name3", Description="Description3"},
+                new AlbumModel() {Name = "Name4", Description="Description4"},
+                new AlbumModel() {Name = "Name5", Description="Description5"}
+            };
+
+            return PartialView("Albums", albums);
+        }
+
+        public ActionResult Favourites()
+        {
+            return Content("likes are here");
+        }
+
+        public ActionResult Settings()
+        {
+            return Content("this is settings");
+        }
+    }
+}
---
+++ Web/Models/ImageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class ImageModel
+    {
+        public byte[] Img { get; set; }
+        public int Likes { get; set; }
+        public ICollection<string> Tags { get; set; }
+    }
+}
